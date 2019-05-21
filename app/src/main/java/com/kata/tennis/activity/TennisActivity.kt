6b654dae19786788fb11b96e7236883635c428fd
sam18892 <sam.yohind@gmail.com>
2019-05-22 04:27:44 +0530

    Initial commit

---
+++ app/src/main/java/com/kata/tennis/activity/TennisActivity.kt
@@ -0,0 +1,13 @@
+package com.kata.tennis.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kata.tennis.R
+
+class TennisActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tennis)
+    }
+}
---
+++ app/src/test/java/com/kata/tennis/TennisViewModelUnitTest.kt
@@ -0,0 +1,12 @@
+package com.kata.tennis
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TennisViewModelUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
