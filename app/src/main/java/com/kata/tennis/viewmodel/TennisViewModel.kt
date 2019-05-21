e7bb9b89f66a5fbdb10cd029c0794b2c00d2b5f3
sam18892 <sam.yohind@gmail.com>
2019-05-22 04:41:29 +0530

    Added test case to check if both players are at love("0") for a new game and added relevant class members in viewModel.

--- app/src/main/java/com/kata/tennis/viewmodel/TennisViewModel.kt
+++ app/src/main/java/com/kata/tennis/viewmodel/TennisViewModel.kt
@@ -4,4 +4,6 @@ import android.arch.lifecycle.ViewModel
 
 class TennisViewModel: ViewModel(){
 
+    var playerOneScore = 0
+    var playerTwoScore = 0
 }
--- app/src/test/java/com/kata/tennis/TennisViewModelUnitTest.kt
+++ app/src/test/java/com/kata/tennis/TennisViewModelUnitTest.kt
@@ -1,12 +1,16 @@
 package com.kata.tennis
 
+import com.kata.tennis.viewmodel.TennisViewModel
 import org.junit.Test
 
 import org.junit.Assert.*
 
 class TennisViewModelUnitTest {
+    private var tennisViewModel = TennisViewModel()
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testShouldCheckIfPlayersPointsAreAtLoveForNewGame() {
+        assertEquals(0, tennisViewModel.playerOneScore)
+        assertEquals(0, tennisViewModel.playerTwoScore)
     }
 }
